eb3bf3682cafa14b9345997bfd4fc8c4db3bfc15
Abdalrahman Alfar <abd0195744@ju.edu.jo>
2023-09-06 12:14:27 +0300

    fifth commit

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,8 @@ export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter()
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,
+  private toastr: ToastrService) { }
   
   register() {
     this.accountService.register(this.model)
@@ -18,7 +20,7 @@ export class RegisterComponent {
         next: _ => {
           this.cancel();
           },
-      error: err => console.log(err)
+      error: err => this.toastr.error(err.error)
     })
   }
 
