a3b2c27a71acad361c4542b8d9caab924f6667c3
Abdalrahman Alfar <abd0195744@ju.edu.jo>
2023-09-09 19:58:46 +0300

    Recommendation ended

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,27 +8,12 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   registerMode = false;
   users : any 
-  constructor(private http: HttpClient) { }
+  constructor() { }
   
   registerToggle() {
     this.registerMode = !this.registerMode
   }
 
-    getUsers() {
-      this.http.get('https://localhost:5001/users')
-      .subscribe({
-        next: r => this.users = r,
-        error: this.handelError,
-        complete: () => console.log('request is completed')
-    })
-    }
-  
-    private handelError(err: any) {
-    console.log("Response Error, Status:", err.status);
-    console.log("Response Error, Status Text:", err.statusText);
-    console.log(err);
-    }
-  
   cancelregisterMode(event: boolean) {
     this.registerMode = event;
   }
