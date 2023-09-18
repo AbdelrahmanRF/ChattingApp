c9c43262e2dad5fb3b9a43c08ce48c66b2a5ed98
AbdelrahmanRF <abd0195744@ju.edu.jo>
2023-09-18 21:18:03 +0300

    add like user feature

--- API/Helpers/PaginationParams.cs
+++ API/Helpers/PaginationParams.cs
@@ -1,6 +1,6 @@
 ﻿namespace API.Helpers;
 
-public class UserParams
+public class PaginationParams
 {
     private const int _maxPageSize = 50;
     public int PageNumber { get; set; } = 1;
@@ -9,10 +9,5 @@ public class UserParams
     {
         get => _pageSize;
         set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
-    }  
-    public string CurrentUsername { get; set; }
-    public string Gender { get; set; }
-    public int MinAge { get; set; } = 18;
-    public int MaxAge { get; set; } = 100;
-    public string OrderBy { get; set; } = "lastActive";
+    }
 }
